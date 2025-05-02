168fa2d91ff39a23e86ecbac75028fd525fc687b
Jose Furtado <josefurtado@Mac.home>
2025-05-02 20:36:16 +0200

    Setup Biome for lint and format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/eslint'],
+  modules: ['@nuxthub/core'],
 
-  // https://devtools.nuxt.com
   devtools: { enabled: true },
 
-  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
-  runtimeConfig: {},
-  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2025-03-01',
 
-  // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {},
-
-  // Development config
-  eslint: {
-    config: {
-      stylistic: {
-        quotes: 'single',
-        commaDangle: 'never',
-      },
-    },
-  },
-});
+})
