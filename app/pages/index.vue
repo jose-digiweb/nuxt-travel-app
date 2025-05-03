57aec411657c10ebed7bc80ecb6f0133b7c80af0
Jose Furtado <josefurtado@Mac.home>
2025-05-03 22:29:22 +0200

    Protected dashboard route server side

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const auth = useAuthStore()
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -10,7 +14,15 @@
           intuitive. Add your travels, manage your itineraries, and stay organized.
         </p>
 
-        <AuthLoginButton />
+        <AuthLoginButton v-if="!auth.user" />
+
+        <NuxtLink v-else to="/dashboard" class="btn btn-primary">
+          <span>
+            Manage your travels
+          </span>
+
+          <Icon name="tabler:map-2" size="24" class="ml-2" />
+        </NuxtLink>
       </div>
     </div>
   </div>
