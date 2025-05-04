85e2a9ef82bcc7e760b2ae543d8717561c2fb6ea
Jose Furtado <josefurtado@Mac.home>
2025-05-04 14:52:54 +0200

    Added dashboard button on the home page when loged in

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const auth = useAuthStore()
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -10,7 +14,15 @@
           intuitive. Add your travels, manage your itineraries, and stay organized.
         </p>
 
-        <AuthLoginButton />
+        <AuthLoginButton v-if="!auth?.user" />
+
+        <NuxtLink to="/dashboard" v-else class="btn btn-primary">
+          <span>
+            My Travels
+          </span>
+
+          <Icon name="tabler:map-pin-2" size="24" class="ml-2" />
+        </NuxtLink>
       </div>
     </div>
   </div>
