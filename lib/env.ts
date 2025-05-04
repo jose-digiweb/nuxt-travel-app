82f3b0ff20e73ff038bb62fed45de59bd4a3e67a
Jose Furtado <josefurtado@Mac.home>
2025-05-04 13:45:58 +0200

    wip

--- lib/env.ts
+++ lib/env.ts
@@ -1,18 +1,18 @@
-import { z } from 'zod'
-import { tryParseEnvZod } from './tryParseEnvZod'
+// import { z } from 'zod'
+// import { tryParseEnvZod } from './tryParseEnvZod'
 
-const EnvSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production']),
-  TURSO_DATABASE_URL: z.string(),
-  TURSO_AUTH_TOKEN: z.string(),
-  BETTER_AUTH_SECRET: z.string(),
-  BETTER_AUTH_URL: z.string(),
-  AUTH_GITHUB_CLIENT_ID: z.string(),
-  AUTH_GITHUB_CLIENT_SECRET: z.string(),
-})
+// const EnvSchema = z.object({
+//   NODE_ENV: z.enum(['development', 'production']),
+//   TURSO_DATABASE_URL: z.string(),
+//   TURSO_AUTH_TOKEN: z.string(),
+//   BETTER_AUTH_SECRET: z.string(),
+//   BETTER_AUTH_URL: z.string(),
+//   AUTH_GITHUB_CLIENT_ID: z.string(),
+//   AUTH_GITHUB_CLIENT_SECRET: z.string(),
+// })
 
-export type EnvSchemaType = z.infer<typeof EnvSchema>
+// export type EnvSchemaType = z.infer<typeof EnvSchema>
 
-tryParseEnvZod(EnvSchema)
+// tryParseEnvZod(EnvSchema)
 
-export default EnvSchema.parse(process.env)
+// export default EnvSchema.parse(process.env)
