143345f22ec55036b582a370669974b729282ced
Jose Furtado <josefurtado@Mac.home>
2025-05-04 12:28:08 +0200

    wip

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,5 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-import { createAuthMiddleware } from 'better-auth/plugins'
 import db from './db'
 import env from './env'
 
@@ -16,19 +15,5 @@ export const auth = betterAuth({
       clientId: env.AUTH_GITHUB_CLIENT_ID,
       clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
     },
-  },
-  hooks: {
-    // biome-ignore lint/suspicious/useAwait: <ass>
-    after: createAuthMiddleware(async (ctx) => {
-      if (ctx.path === '/get-session') {
-        if (!ctx.context.session) {
-          return {
-            session: null,
-            user: null,
-          }
-        }
-        return ctx.json(ctx.context.session)
-      }
-    }),
   },
 })
