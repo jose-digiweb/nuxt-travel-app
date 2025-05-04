c587f4c4157b92f529d25bc7ec8adc092d539b0a
Jose Furtado <josefurtado@Mac.home>
2025-05-04 20:54:25 +0200

    handled better auth response when user is logged out

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { createAuthMiddleware } from 'better-auth/plugins'
 import db from './db'
 import env from './env'
 
@@ -7,13 +8,24 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'sqlite',
   }),
-  advanced: {
-    generateId: false,
-  },
   socialProviders: {
     github: {
       clientId: env.AUTH_GITHUB_CLIENT_ID,
       clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
     },
+  },
+  hooks: {
+    // biome-ignore lint/suspicious/useAwait: <skip>
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === '/get-session') {
+        if (!ctx.context.session) {
+          return ctx.json({
+            session: null,
+            user: null,
+          })
+        }
+        return ctx.json(ctx.context.session)
+      }
+    }),
   },
 })
