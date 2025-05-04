eb243755dcb62f4c571392fdaf7e1605a9796707
Jose Furtado <josefurtado@Mac.home>
2025-05-04 14:30:38 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import env from './lib/env'
+import './lib/env'
 
 // Dependencies
 import tailwindcss from '@tailwindcss/vite'
@@ -25,16 +25,5 @@ export default defineNuxtConfig({
     experimental: {
       openAPI: true,
     },
-  },
-
-  runtimeConfig: {
-    NODE_ENV: env.NODE_ENV,
-    TURSO_DATABASE_URL: env.TURSO_DATABASE_URL,
-    TURSO_AUTH_TOKEN: env.TURSO_AUTH_TOKEN,
-    BETTER_AUTH_SECRET: env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: env.BETTER_AUTH_URL,
-    AUTH_GITHUB_CLIENT_ID: env.AUTH_GITHUB_CLIENT_ID,
-    AUTH_GITHUB_CLIENT_SECRET: env.AUTH_GITHUB_CLIENT_SECRET,
-    NUXT_HUB_PROJECT_KEY: env.NUXT_HUB_PROJECT_KEY,
   },
 })
