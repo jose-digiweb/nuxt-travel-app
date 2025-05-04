8bc3a1e07750acad7ba11a8dfea5095fef9d6239
Jose Furtado <josefurtado@Mac.home>
2025-05-04 15:47:36 +0200

    test

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,10 +1,6 @@
-import { createAuthClient } from 'better-auth/vue'
-
-const authClient = createAuthClient()
-
-export default defineNuxtRouteMiddleware(async (to) => {
-  const { data: session } = await authClient.useSession(useFetch)
-  if (!session.value && to.path === '/dashboard') {
+export default defineNuxtRouteMiddleware((to) => {
+  const auth = useAuthStore()
+  if (!auth?.user && to.path === '/dashboard') {
     return navigateTo('/')
   }
 })
--- server/api/[...auth].ts
+++ server/api/[...auth].ts
@@ -1,5 +1,7 @@
 import { auth } from '../../app/lib/auth'
 
 export default defineEventHandler((event) => {
+  const env = process.env.AUTH_GITHUB_CLIENT_ID
+  console.log('EBV ==>> ', env)
   return auth.handler(toWebRequest(event))
 })
